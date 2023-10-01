6615296757b1c1b674616e4ee8fba634dbe3cfcb
Hunter Johns <hunter.johns@bsu.edu>
2023-10-01 18:14:56 -0400

    Add TextField 'articleInput' to accept user input, store user input from articleInput

--- src/main/java/edu/bsu/cs222/GUI.java
+++ src/main/java/edu/bsu/cs222/GUI.java
@@ -2,9 +2,11 @@ package edu.bsu.cs222;
 
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -26,16 +28,22 @@ public class GUI extends Application{
         Label instructionText = new Label("Please insert the name of a Wikipedia article you wish to check the revision history of.");
         Label outputText = new Label("This is where the output will go.");
 
+        TextField articleInput = new TextField();
+
         search = new Button();
         search.setText("Search");
-        search.setOnAction(e -> primaryStage.setScene(output));
+        search.setOnAction(e -> {
+            primaryStage.setScene(output);
+            String input =articleInput.getText();
+        });
 
         brokenButton = new Button();
         brokenButton.setText("This is an empty result");
-        brokenButton.setOnAction(e -> AlertBox.output("Oops", "Invalid entry."));
+        brokenButton.setOnAction(e -> AlertBox.output("Oops", "Invalid input"));
 
         VBox layoutInput = new VBox(50);
-        layoutInput.getChildren().addAll(instructionText, search, brokenButton);
+        layoutInput.getChildren().addAll(instructionText, articleInput, search, brokenButton);
+        layoutInput.setAlignment(Pos.CENTER_LEFT);
 
         StackPane layoutOutput = new StackPane();
         layoutOutput.getChildren().add(outputText);
